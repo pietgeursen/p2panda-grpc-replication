a4f2249921048020936fafafde82ed1a65dde750
Piet Geursen <piet@cryptovoxels.com>
2021-12-03 18:38:55 +1300

    latest

--- README.md
+++ README.md
@@ -13,3 +13,15 @@
   - change the address / port to `localhost:50051`
   - press play
 
+## Design intentions
+
+- Allow resource constrained devices to participate. We do this by defining a simple core api based on polling with optional extensions that improve bandwidth and allow for live updates.
+- Prefer off the shelf solutions to lower barrier to adoption. We think gRPC is the best choice for this.
+- Let the client stay in control by asking for what it needs.
+  - Streaming updates from the server should only broadcast lightweight messages that notify that something has happened, not what has happened. Clients decide when or if they want to request the actual change. This makes it harder to overwhelm the client if a huge number of large messages would otherwise be pushed on the client.
+- Prefer apis that work with collections of items rather than one by one. This enables batching / throttling of workloads.
+
+
+## Replication RPC
+
+
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use futures::Stream;
+use std::pin::Pin;
 use tonic::{transport::Server, Request, Response, Status};
 
 use replication::replication_server::{Replication, ReplicationServer};
@@ -7,41 +9,30 @@ pub mod replication {
     tonic::include_proto!("replication"); // The string specified here must match the proto package name
 }
 
+type ResponseStream =
+    Pin<Box<dyn Stream<Item = Result<NewLogHeightsStreamResponse, Status>> + Send>>;
+
 #[derive(Debug, Default)]
 pub struct MyReplication {}
 
 #[tonic::async_trait]
 impl Replication for MyReplication {
-
-    async fn get_author_aliases(
-        &self,
-        request: Request<GetAuthorAliasesRequest>,
-    ) -> Result<Response<GetAuthorAliasesResponse>, Status> {
-        println!("Got a request: {:?}", request);
-
-        let error = AuthorAliasesNotSupportedError{};
-        let error_type = Some(get_author_aliases_response::errors::ErrorType::AuthorAliasesNotSupported(error));
-
-        let reply = GetAuthorAliasesResponse { response: Some(get_author_aliases_response::Response::Error(get_author_aliases_response::Errors{error_type})) };
-        Ok(Response::new(reply))
-    }
     async fn get_all_entries_by_authors(
         &self,
         request: Request<GetAllEntriesByAuthorsRequest>,
     ) -> Result<Response<GetEntriesResponse>, Status> {
         println!("Got a request: {:?}", request);
 
-        let reply = GetEntriesResponse { response: None };
+        let reply = GetEntriesResponse { entries: vec![] };
         Ok(Response::new(reply))
     }
-
     async fn get_single_entry(
         &self,
         request: Request<GetSingleEntryRequest>,
     ) -> Result<Response<GetEntriesResponse>, Status> {
         println!("Got a request: {:?}", request);
 
-        let reply = GetEntriesResponse { response: None };
+        let reply = GetEntriesResponse { entries: vec![] };
         Ok(Response::new(reply))
     }
 
@@ -51,49 +42,50 @@ impl Replication for MyReplication {
     ) -> Result<Response<GetEntriesResponse>, Status> {
         println!("Got a request: {:?}", request);
 
-        let reply = GetEntriesResponse { response: None };
+        let reply = GetEntriesResponse { entries: vec![] };
         Ok(Response::new(reply))
     }
 
-    async fn get_entries_delta(
+    async fn get_log_height_deltas(
         &self,
-        request: Request<GetEntriesDeltaRequest>,
-    ) -> Result<Response<GetEntriesDeltaResponse>, Status> {
+        request: Request<GetLogHeightDeltasRequest>,
+    ) -> Result<Response<GetLogHeightDeltasResponse>, Status> {
         println!("Got a request: {:?}", request);
 
-        let reply = GetEntriesDeltaResponse { response: None };
+        let reply = GetLogHeightDeltasResponse {
+            author_known_log_heights: vec![],
+        };
         Ok(Response::new(reply))
     }
 
+    async fn new_log_heights_stream(
+        &self,
+        _request: Request<NewLogHeightsStreamRequest>,
+    ) -> Result<Response<Self::NewLogHeightsStreamStream>, Status> {
+        Err(Status::unimplemented(
+            "New log heights streams are not supported",
+        ))
+    }
 
+    type NewLogHeightsStreamStream = ResponseStream;
 
+    async fn set_author_aliases(
+        &self,
+        request: Request<SetAuthorAliasesRequest>,
+    ) -> Result<Response<SetAuthorAliasesResponse>, Status> {
+        println!("Got a request: {:?}", request);
 
-//    async fn get_entries(
-//        &self,
-//        request: Request<GetEntriesRequest>,
-//    ) -> Result<Response<GetEntriesResponse>, Status> {
-//        println!("Got a request: {:?}", request);
-//
-//        let entries = vec![];
-//
-//        let reply = GetEntriesResponse { entries };
-//        Ok(Response::new(reply))
-//    }
-//
-//    async fn get_entries_delta(
-//        &self,
-//        request: Request<GetEntriesDeltaRequest>,
-//    ) -> Result<Response<GetEntriesDeltaResponse>, Status> {
-//        println!("Got a request: {:?}", request);
-//        println!("author: {:?}", request.into_inner().author_known_log_heights);
-//
-//
-//        let author_known_log_heights = vec![];
-//        let reply = GetEntriesDeltaResponse {
-//            author_known_log_heights,
-//        };
-//        Ok(Response::new(reply))
-//    }
+        Err(Status::unimplemented("Author aliases are not supported"))
+    }
+
+    async fn check_author_alias_uuid_is_valid(
+        &self,
+        request: Request<CheckAuthorAliasUuidIsValidRequest>,
+    ) -> Result<Response<CheckAuthorAliasUuidIsValidResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        Err(Status::unimplemented("Author aliases are not supported"))
+    }
 }
 
 #[tokio::main]
@@ -102,11 +94,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let greeter = MyReplication::default();
     let service = ReplicationServer::new(greeter);
 
-
-    Server::builder()
-        .add_service(service)
-        .serve(addr)
-        .await?;
+    Server::builder().add_service(service).serve(addr).await?;
 
     Ok(())
 }
